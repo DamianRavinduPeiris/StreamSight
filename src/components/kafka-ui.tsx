6b81aa53044901e28ec10d85fa2bc541e1673ee7
DamianRavinduPeiris <drpeiris3@gmail.com>
2025-03-11 14:22:27 +0530

    Topic search fix added.

--- src/components/kafka-ui.tsx
+++ src/components/kafka-ui.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import Alert from "./alert/Alert";
+import NavBar from "./ui/NavBar";
+
 
 const socket = io("http://localhost:5000");
 
@@ -17,6 +19,18 @@ export default function KafkaUI() {
   const [searchQuery, setSearchQuery] = useState("");
   const [isProducerDropdownOpen, setProducerDropdownOpen] = useState(false);
   const [isConsumerDropdownOpen, setConsumerDropdownOpen] = useState(false);
+  const [producerSearchQuery, setProducerSearchQuery] = useState("");
+  const [consumerSearchQuery, setConsumerSearchQuery] = useState("");
+
+  const filteredProducerTopics = topics.filter((t) =>
+    t.toLowerCase().includes(producerSearchQuery.toLowerCase())
+  );
+
+  const filteredConsumerTopics = topics.filter((t) =>
+    t.toLowerCase().includes(consumerSearchQuery.toLowerCase())
+  );
+
+
 
   useEffect(() => {
     socket.on("status", (status) => {
@@ -103,155 +117,157 @@ export default function KafkaUI() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center px-6 py-8">
-      <h1 className="text-4xl font-bold tracking-wide mb-10">ALMSafeStream</h1>
+    <div>
+      <NavBar />
+      <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center px-6 py-8">
 
-      {showAlert && <Alert msg={alertMessage} />}
 
-      <div className="w-full max-w-4xl space-y-6">
-        <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-          <h2 className="text-2xl font-semibold mb-4">Produce Event</h2>
-          <div className="space-y-4">
-            <div className="relative">
-              <input
-                type="text"
-                placeholder="Search and type topic"
-                value={topic}
-                onChange={(e) => {
-                  setTopic(e.target.value);
-                  setSearchQuery(e.target.value);
-                }}
-                onFocus={() => {
-                  setProducerDropdownOpen(true);
-                  setConsumerDropdownOpen(false);
-                }}
-                className="w-full bg-gray-700 text-white px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
-              />
-              {isProducerDropdownOpen && (
-                <div className="absolute w-full bg-gray-700 text-white rounded-lg mt-1 z-10 max-h-60 overflow-y-auto">
-                  {filteredTopics.length > 0 ? (
-                    filteredTopics.map((topicItem, index) => (
-                      <div
-                        key={index}
-                        onClick={() => handleProducerTopicSelect(topicItem)}
-                        className="px-4 py-2 cursor-pointer hover:bg-gray-600"
-                      >
-                        {highlightQuery(topicItem)}
+        {showAlert && <Alert msg={alertMessage} />}
+
+        <div className="w-full max-w-4xl space-y-6">
+          <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
+            <h2 className="text-2xl font-semibold mb-4">Produce Event</h2>
+            <div className="space-y-4">
+              <div className="relative">
+                <input
+                  type="text"
+                  placeholder="Search and type topic"
+                  value={topic}
+                  onChange={(e) => {
+                    setTopic(e.target.value);
+                    setProducerSearchQuery(e.target.value);
+                  }}
+                  onFocus={() => {
+                    setProducerDropdownOpen(true);
+                    setConsumerDropdownOpen(false);
+                  }}
+                  className="w-full bg-gray-700 text-white px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
+                />
+                {isProducerDropdownOpen && (
+                  <div className="absolute w-full bg-gray-700 text-white rounded-lg mt-1 z-10 max-h-60 overflow-y-auto">
+                    {filteredProducerTopics.length > 0 ? (
+                      filteredProducerTopics.map((topicItem, index) => (
+                        <div
+                          key={index}
+                          onClick={() => handleProducerTopicSelect(topicItem)}
+                          className="px-4 py-2 cursor-pointer hover:bg-gray-600"
+                        >
+                          {highlightQuery(topicItem)}
+                        </div>
+                      ))
+                    ) : (
+                      <div className="px-4 py-2 text-gray-400">
+                        No topics found
                       </div>
-                    ))
-                  ) : (
-                    <div className="px-4 py-2 text-gray-400">
-                      No topics found
-                    </div>
-                  )}
-                </div>
-              )}
-            </div>
+                    )}
+                  </div>
+                )}
+              </div>
 
-            <textarea
-              placeholder="Enter message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              onBlur={() => formatJson(message)}
-              className="w-full h-32 bg-gray-700 text-white px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
-            />
-            <button
-              onClick={handleProduce}
-              disabled={!topic || !message}
-              className={`px-6 py-2 rounded-lg text-white font-medium transition ${
-                topic && message
+              <textarea
+                placeholder="Enter message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                onBlur={() => formatJson(message)}
+                className="w-full h-32 bg-gray-700 text-white px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
+              />
+              <button
+                onClick={handleProduce}
+                disabled={!topic || !message}
+                className={`px-6 py-2 rounded-lg text-white font-medium transition ${topic && message
                   ? "bg-blue-600 hover:bg-blue-500"
                   : "bg-gray-600 cursor-not-allowed"
-              }`}
-            >
-              Produce Event
-            </button>
+                  }`}
+              >
+                Produce Event
+              </button>
+            </div>
           </div>
-        </div>
 
-        {shouldShowConsumableEvents && (
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-            {consumingTopic ? (
-              <>
+          {shouldShowConsumableEvents && (
+            <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
+              {consumingTopic ? (
+                <>
+                  <h2 className="text-2xl font-semibold mb-4">
+                    Consuming from : {consumingTopic}
+                  </h2>
+                  <span className="loading loading-infinity loading-lg"></span>
+                </>
+              ) : (
                 <h2 className="text-2xl font-semibold mb-4">
-                  Consuming from : {consumingTopic}
+                  Select a topic to consume.
                 </h2>
-                <span className="loading loading-infinity loading-lg"></span>
-              </>
-            ) : (
-              <h2 className="text-2xl font-semibold mb-4">
-                Select a topic to consume.
-              </h2>
-            )}
+              )}
 
-            <div className="relative mb-4">
+              <div className="relative mb-4">
+                <input
+                  type="text"
+                  placeholder="Search consuming topics"
+                  value={consumingTopic}
+                  onFocus={() => {
+                    setConsumerDropdownOpen(true);
+                    setProducerDropdownOpen(false);
+                  }}
+                  onChange={(e) => {
+                    setConsumingTopic(e.target.value);
+                    setConsumerSearchQuery(e.target.value);
+                  }}
+                  className="w-full bg-gray-700 text-white px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
+                />
+                {isConsumerDropdownOpen && (
+                  <div className="absolute w-full bg-gray-700 text-white rounded-lg mt-1 z-10 max-h-60 overflow-y-auto">
+                    {filteredConsumerTopics.length > 0 ? (
+                      filteredConsumerTopics.map((topicItem, index) => (
+                        <div
+                          key={index}
+                          onClick={() => handleConsumerTopicSelect(topicItem)}
+                          className="px-4 py-2 cursor-pointer hover:bg-gray-600"
+                        >
+                          {highlightQuery(topicItem)}
+                        </div>
+                      ))
+                    ) : (
+                      <div className="px-4 py-2 text-gray-400">
+                        No topics found
+                      </div>
+                    )}
+                  </div>
+                )}
+              </div>
+
               <input
                 type="text"
-                placeholder="Search consuming topics"
-                value={consumingTopic}
-                onFocus={() => {
-                  setConsumerDropdownOpen(true);
-                  setProducerDropdownOpen(false);
-                }}
-                onChange={(e) => {
-                  setConsumingTopic(e.target.value);
-                  setSearchQuery(e.target.value);
-                }}
-                className="w-full bg-gray-700 text-white px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
+                placeholder="Search consumed events..."
+                value={searchQuery}
+                onChange={handleSearch}
+                className="w-full bg-gray-700 text-white px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 mb-4"
               />
-              {isConsumerDropdownOpen && (
-                <div className="absolute w-full bg-gray-700 text-white rounded-lg mt-1 z-10 max-h-60 overflow-y-auto">
-                  {filteredTopics.length > 0 ? (
-                    filteredTopics.map((topicItem, index) => (
-                      <div
-                        key={index}
-                        onClick={() => handleConsumerTopicSelect(topicItem)}
-                        className="px-4 py-2 cursor-pointer hover:bg-gray-600"
-                      >
-                        {highlightQuery(topicItem)}
-                      </div>
-                    ))
-                  ) : (
-                    <div className="px-4 py-2 text-gray-400">
-                      No topics found
+
+              <div className="overflow-y-auto max-h-64 bg-gray-700 rounded-lg">
+                {filteredEvents.length > 0 ? (
+                  filteredEvents.map((event, index) => (
+                    <div key={index} className="p-4 border-b border-gray-600">
+                      <pre className="whitespace-pre-wrap text-sm">
+                        {highlightQuery(event)}
+                      </pre>
                     </div>
-                  )}
-                </div>
-              )}
+                  ))
+                ) : (
+                  <p className="text-gray-400 text-center py-4">
+                    No events found
+                  </p>
+                )}
+              </div>
             </div>
+          )}
 
-            <input
-              type="text"
-              placeholder="Search consumed events..."
-              value={searchQuery}
-              onChange={handleSearch}
-              className="w-full bg-gray-700 text-white px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 mb-4"
-            />
-
-            <div className="overflow-y-auto max-h-64 bg-gray-700 rounded-lg">
-              {filteredEvents.length > 0 ? (
-              filteredEvents.map((event, index) => (
-                <div key={index} className="p-4 border-b border-gray-600">
-                <pre className="whitespace-pre-wrap text-sm">
-                  {highlightQuery(event)}
-                </pre>
-                </div>
-              ))
-              ) : (
-              <p className="text-gray-400 text-center py-4">
-                No events found
-              </p>
-              )}
-            </div>
-          </div>
-        )}
-
-        <button
-          onClick={() => setConsumableEventsStatus(!shouldShowConsumableEvents)}
-          className="px-6 py-2 bg-gray-700 hover:bg-gray-600 text-white rounded-lg font-medium"
-        >
-          {shouldShowConsumableEvents ? "Hide Events" : "Consume Events"}
-        </button>
+          <button
+            onClick={() => setConsumableEventsStatus(!shouldShowConsumableEvents)}
+            className="px-6 py-2 bg-gray-700 hover:bg-gray-600 text-white rounded-lg font-medium"
+          >
+            {shouldShowConsumableEvents ? "Hide Events" : "Consume Events"}
+          </button>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/ui/NavBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>
+        <div className="navbar bg-base-100">
+  <div className="navbar-start">
+  </div>
+  <div className="navbar-center">
+    <a className="btn btn-ghost text-xl">ALMSafeStream</a>
+  </div>
+  <div className="navbar-end">
+    <button className="btn btn-ghost btn-circle">
+    </button>
+    <button className="btn btn-ghost btn-circle">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBKwnLWefTnomeBdBhvPhIX1nosV1iARxAdQ&s" alt="" className='w-10 h-10 rounded-full' />
+    </button>
+  </div>
+</div>
+      
+    </div>
+  )
+}
