c1c55facdd5417ba317c9f58154277c395f56646
DamianRavinduPeiris <drpeiris3@gmail.com>
2025-03-25 17:33:54 +0530

    Updated the header.

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function NavBar() {
   return (
     <div>
@@ -7,14 +5,13 @@ export default function NavBar() {
   <div className="navbar-start">
   </div>
   <div className="navbar-center">
-    <a className="btn btn-ghost text-xl">ALMSafeStream</a>
+    <a className="btn btn-ghost text-xl">Kafka Stream</a>
   </div>
   <div className="navbar-end">
     <button className="btn btn-ghost btn-circle">
     </button>
-    <button className="btn btn-ghost btn-circle">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBKwnLWefTnomeBdBhvPhIX1nosV1iARxAdQ&s" alt="" className='w-10 h-10 rounded-full' />
-    </button>
+
+    
   </div>
 </div>
       
